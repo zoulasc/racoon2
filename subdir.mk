b887ad9f51d83c63c01e374eafb9a46dd63256a1
Christos Zoulas <christos@zoulas.com>
2018-07-05 22:27:30 -0400

    re-organize the samples files

---
+++ subdir.mk
@@ -0,0 +1,56 @@
+all: build
+
+build: build-recursive
+
+build-recursive: .MAKE
+	@for d in $(SUBDIR) ; do \
+		if test -d $$d ; then \
+			echo "===> Change directory to $$d" ; \
+			(cd $$d && $(MAKE)) || exit 1 ; \
+		fi ; done
+
+install: install-recursive
+
+install-recursive: .MAKE
+	@for d in $(SUBDIR) ; do \
+		if test -d $$d ; then \
+			echo "===> Change directory to $$d" ; \
+			(cd $$d && $(MAKE) install) || exit 1 ; \
+		fi ; done
+
+# pmake does not require '-' before MAKEFLAGS but gmake does.
+# Recent makes automatically pass command line options to submakes via
+# environment variables, so we actually don't need MFLAGS nor MAKEFLAGS here.
+
+clean: clean-recursive
+
+clean-recursive: .MAKE
+	-@for d in $(ALLSUBDIR) ; do \
+		if test -d $$d; then \
+			echo "===> Change directory to $$d" ; \
+			(cd $$d && $(MAKE) clean) \
+		fi ; done
+
+distclean-top:
+	-rm -f Makefile config.cache config.status config.log
+
+distclean: distclean-recursive distclean-top
+
+distclean-recursive: .MAKE
+	-@for d in $(ALLSUBDIR) ; do \
+		if test -d $$d; then \
+			echo "===> Change directory to $$d" ; \
+			(cd $$d && $(MAKE) distclean) \
+		fi ; done
+
+depend: depend-recursive
+
+depend-recursive: .MAKE
+	for d in $(SUBDIR) ; do \
+		if test -d $$d; then \
+			echo "===> Change directory to $$d" ; \
+			(cd $$d && $(MAKE) depend) || exit 1 ; \
+		fi ; done
+
+# dummy target for gmake.  gmake magically works recursively with -n.
+.MAKE:
